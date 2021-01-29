0aa1d3a9c588331078e22cb51ab0eec37b587756
KIRUBAKARAN MUTHUPALANI <kiru@KIRUBAKARANsMBP.home>
2021-01-29 14:24:45 +0000

    server config

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const classRouter = require('./routes/classRoutes');
+const userRouter = require('./routes/userRoutes');
+
+// const cors = require('cors')
+// const {MongoClient} = require('mongodb')
+// let db;
+const app = express();
+
+// Middlewares
+// using environment variable to determine using certain middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json()); // Middleware to access the req.body json file (body-parser)
+// app.use(express.static(`${__dirname}/public`))
+// app.use(cors())
+
+app.use('/api/v1/tickets', classRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
