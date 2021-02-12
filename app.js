b534298a90f91bacb97ae370181a3d866447a640
KIRUBAKARAN MUTHUPALANI <kiru@KIRUBAKARANsMBP.home>
2021-02-12 15:27:02 +0000

    Initial Error Handling

--- app.js
+++ app.js
@@ -22,4 +22,12 @@ app.use(express.json()); // Middleware to access the req.body json file (body-pa
 app.use('/api/v1/tickets', classRouter);
 app.use('/api/v1/users', userRouter);
 
+// Error handling for undeclared URLs
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `can't find ${req.originalUrl} on this server`,
+  });
+});
+
 module.exports = app;
